2798f67af41544e72d2c2f907a05c14a7289beea
Nita Andriani <139097082+Nitaan@users.noreply.github.com>
2024-01-02 14:12:18 +0700

    Update clust_credit.py

--- clust_credit.py
+++ clust_credit.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
-import matplotlib.font_manager
 import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
